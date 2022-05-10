c34de14cebdf9fde801d042c4efae9c374573c42
Dalil <71337691+dalildal@users.noreply.github.com>
2022-05-10 12:38:11 +0200

    update virement.h

--- virement.h
+++ virement.h
@@ -1,7 +1,11 @@
+#ifndef _VIREMENT_H_
+#define _VIREMENT_H_
 /* struct virement used between server and client */
 typedef struct
 {
   int num_destinataire;
   int num_expediteur;
   double montant;
-} Payment;
+} Virement;
+
+#endif
