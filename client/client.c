ab7e604999aac40b3f384e022409a62846b83ee8
haves9723 <christian9723@hotmail.com>
2022-05-12 10:55:39 +0200

    client change v1

--- client/client.c
+++ client/client.c
@@ -20,9 +20,9 @@
 #define MAX_SIZE_PIPE 10
 
 void minuterie(void *delay);
+void virementRec(void *delay);
 
-
-//Variable
+// Variable
 int pipefd[2];
 
 // PRE: ServierIP : a valid IP address
@@ -40,15 +40,15 @@ int initSocketClient(char ServerIP[16], int Serverport)
 int main(int argc, char **argv)
 {
     printf("test 1");
-    //creation de la pipe
+    // creation de la pipe
     int ret = spipe(pipefd);
     checkNeg(ret, "pipe error");
 
     StructMessage msg;
     Virement virement;
-    
+
     // msg.messageText[ret - 1] = '\0';
-    char* adr = argv[1];
+    char *adr = argv[1];
     int port = atoi(argv[2]);
     int num = atoi(argv[3]);
     int delay = atoi(argv[4]);
@@ -56,50 +56,61 @@ int main(int argc, char **argv)
     virement.num_destinataire = 4;
     virement.num_expediteur = 5;
     virement.montant = 45;
-    
-    //ferme le pipe de lecture
+
+    // ferme le pipe de lecture
     int retour = close(pipefd[0]);
     checkNeg(retour, "close error");
 
     int ppidMinute = fork_and_run1(minuterie, &delay);
-    
+    int ppidVirementRecurrent = fork_and_run0(virementRec);
+
+    // int pipeMinuterie = fork_and_run1();
+
+    char cmd[BUFFER_SIZE];
+
+    while (true)
+    {
+        sread(0, cmd, BUFFER_SIZE);
+        printf("%d", cmd[0]);
+        if (cmd[0] == '+')
+        {
+        }
+        else if (cmd[0] == '*')
+        {
+            
+        }
+
+        else if (cmd[0] == 'q')
+        {
+            skill(ppidMinute, SIGQUIT);
+            skill(getpid(), SIGQUIT);
+            exit(0);
+        }
+    }
+
     int sockfd = initSocketClient(adr, port);
     swrite(sockfd, &msg, sizeof(msg));
     swrite(sockfd, &virement, sizeof(virement));
 
     printf("%d minuterie %d", num, delay);
-    // int pipeMinuterie = fork_and_run1();
-
-    char cmd[BUFFER_SIZE];
-
-    while(true) {
-
-        sread(0,cmd, BUFFER_SIZE);
-         printf("%d", cmd[0]);
-        if (cmd[0] == 'q')
-        {
-            skill(ppidMinute, SIGQUIT);
-            skill(getpid(),SIGQUIT);
-            exit(0);
-        }
-    }
 
     sclose(sockfd);
     return 0;
 }
 
 void minuterie(void *delay)
-{   
+{
     /*Cast de void à int*/
-    int* duration = delay;
+    int *duration = delay;
     int durationInt = *duration;
     /*Fermeture de la pipe en lecture*/
     sclose(pipefd[0]);
-    while(true){
+    while (true)
+    {
         sleep(durationInt);
         char buffer[MAX_SIZE_PIPE];
         /*b pour battement*/
         buffer[0] = 'b';
-        nwrite(pipefd[1], &buffer, MAX_SIZE_PIPE*sizeof(char));
+        nwrite(pipefd[1], &buffer, MAX_SIZE_PIPE * sizeof(char));
     }
 }
